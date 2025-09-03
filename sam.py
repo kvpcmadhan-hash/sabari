70db5d8864398f2a0573b6a1ebec02d6848c9749
kvpcmadhan-hash <kvpcmadhan@gmail.com>
2025-09-03 10:55:49 +0530

    Add files via upload

---
+++ sam.py
@@ -0,0 +1,8 @@
+def add(a, b):
+    return a + b
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+result = add(num1, num2)
+print(f"The sum of {num1} and {num2} is {result}")
